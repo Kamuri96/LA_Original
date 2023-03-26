f6091853430985c3f2ccdc82e3a993c1fa709cec
Kamuri96 <mitsu20010909@gmail.com>
2023-03-26 21:21:37 +0900

    BGM,Effect,Mask

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -5,17 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "key")
